6d74661a1bd0be7dedc2ac15f9176c8807cdbb46
Kototap <106796386+kototap@users.noreply.github.com>
2022-08-19 08:17:29 +0000

    会員登録画面のView

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  
-  scope module: public do
+
+  scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about'
     resources :items, only: [:index, :show]
-    
+
     resources :customers do
       get '/my_page' => 'customers#show'
       get '/information/edit' => 'customers#edit'
@@ -21,21 +21,21 @@ Rails.application.routes.draw do
       get '/unsubscribe' => 'customers#unsubscribe'
       patch '/withdraw' => 'customers#withdraw'
     end
-    
+
     resources :cart_items, only: [:index, :update, :create, :destroy] do
       delete 'cart_items/all_destroy' => 'cart_items#all_destroy'
     end
-    
+
     resources :orders, only: [:new, :create, :index, :show] do
       post 'orders/confirm' => 'orders#confirm'
       get 'orders/complete' => 'orders#complete'
     end
-    
+
     resources :shipping_addresses, only: [:index, :edit, :create, :update, :destroy]
-  
+
   end
-  
-  
+
+
   namespace :admin do
     get '/' => 'homes#top'
     resources :items, except: [:destroy]
