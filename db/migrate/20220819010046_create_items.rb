e9beb6d94f42496ef644e31f9a00475a8ce99591
Kototap <106796386+kototap@users.noreply.github.com>
2022-08-19 04:53:34 +0000

    [Add]i18n-for-enum

--- db/migrate/20220819010046_create_items.rb
+++ db/migrate/20220819010046_create_items.rb
@@ -5,8 +5,8 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.string :name, null: false
       t.text :explanation, null: false
       t.integer :price_without_tax, null: false
-      t.boolean :is_sale, null: false, default: 
-      
+      t.boolean :is_sale, null: false, default: true
+
       t.timestamps
     end
   end
