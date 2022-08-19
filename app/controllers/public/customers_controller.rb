62e2c7caa40de962e9f8b0896cd033f8c064c2b0
Kototap <106796386+kototap@users.noreply.github.com>
2022-08-19 16:45:41 +0000

    [Add]edit-page

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,9 +4,13 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customer_my_page_path(@customer.id)
   end
 
   def unsubscribe
@@ -14,4 +18,12 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :postal_code, :phone_number, :address)
+  end
+
 end
