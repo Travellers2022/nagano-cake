531a852716d1d3e67eac409f484c14e297ebc564
EC2 Default User <ec2-user@ip-172-31-38-29.ap-northeast-1.compute.internal>
2022-08-20 02:23:24 +0000

    Add商品一覧画面と商品情報の新規登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page]).per(3)
   end
 
   def new
@@ -8,6 +9,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @genre = Genre.all
+    @item.save
+    redirect_to admin_item_path(@item)
+
   end
 
   def show
@@ -17,5 +23,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def
+    item_params
+    params.require(:item).permit(:item_image, :name, :explanation, :genre_id, :price_without_tax, :is_sale)
   end
 end
