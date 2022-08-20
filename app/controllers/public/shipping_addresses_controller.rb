9e9e4efb341b870cb8bc6d381c5d3d6688e429e9
Takuo27 <103346206+Takuo27@users.noreply.github.com>
2022-08-20 01:11:42 +0000

    about画面と配送先画面の作成

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,16 +1,54 @@
 class Public::ShippingAddressesController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+  
+  
   def index
+    @shipping_address = ShippingAddress.new
+    @shipping_addresses = current_customer.shipping_addresses.all
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
   end
 
   def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    @shipping_address.customer_id = current_customer.id
+    if @shipping_address.save
+      flash[:notice] = "新しい配送先を登録しました"
+      redirect_to　 shipping_addresses_path 
+    else
+      @shipping_addresses = current_customer.shipping_addresses.all
+      render :index
+    end
   end
 
   def update
+    @shipping_address.find(params[:id])
+    if @shipping_address.update
+     flash[:notice] = "配送先を変更しました。"
+     redirect_to  shipping_addresses_path
+   else
+     @shipping_addresses = current_customer.shipping_addresses.all
+     render "edit"
+    end
   end
+  
 
   def destroy
+    @shipping_address = ShippingAddress.find(params[:id])
+    if @shipping_address.destroy
+      redirect_to shipping_addresses_path
+    end
   end
+  
+  private
+  
+  def shipping_address_params
+    params.require(:shipping_address).permit(:name,:address,:postal_code)
+  end
+
 end
+
