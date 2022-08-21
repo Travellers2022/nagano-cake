1f3ebb17f602a392f0926bd4f0d34a45aab5fbaf
EC2 Default User <ec2-user@ip-172-31-38-29.ap-northeast-1.compute.internal>
2022-08-21 07:20:11 +0000

    Add カート内商品全削除

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -12,6 +12,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def all_destroy
+    @cart_items = CartItem.all
+    @cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   def create
