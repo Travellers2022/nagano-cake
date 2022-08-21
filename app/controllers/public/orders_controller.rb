20a449c82a4c6835bd20e5c62cd0852bdc65b462
Takuo27 <103346206+Takuo27@users.noreply.github.com>
2022-08-21 06:53:30 +0000

    注文情報入力の作成途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,23 +4,36 @@ class Public::OrdersController < ApplicationController
   
   def new
     @order = Order.new
-    @shipping_addresses = ShippingAddress.where(customer: current_customer)
+    @customer = Customer.find(current_customer.id)
+    @shipping_addresses = @customer.shipping_addresses
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @shipping_address = ShippingAddress.find(params[:order][:shipping_address_id])
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
   end
 
   def complete
   end
 
   def create
-   @order = current_customer.order.new(order_params)
-
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id 
+    @order.save
+    redirect_to complete_path
   end
 
   def index
   end
 
   def show
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:name, :address, :payment_method, :postal_code, :bill)
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,8 @@
 class OrderItem < ApplicationRecord
 
   enum production_status: { cant: 0, wait: 1, now: 2, finish: 3 }
-
+  
+  
+  belongs_to :order
+  belongs_to :item
 end
