3935d4e495fe58633a6143490e52aa5ea7809fa7
Kototap <106796386+kototap@users.noreply.github.com>
2022-08-23 00:15:11 +0000

    カートアイテム途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -31,7 +31,7 @@ class Public::CartItemsController < ApplicationController
     @cart_items = CartItem.all
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
-
+    redirect_to cart_items_path
   end
 
   def destroy
