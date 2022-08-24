c77fefe7c1467d535a4036a7acfbbecdace5b3df
Takuo27 <103346206+Takuo27@users.noreply.github.com>
2022-08-24 08:20:07 +0000

    ステータス連動

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -6,21 +6,26 @@ class Admin::OrderItemsController < ApplicationController
     
     # @is_make = true
     
-    @order_item.update(production_status: params[:order_item][:production_status])
+    # @order_item.update(production_status: params[:order_item][:production_status])
+     @order_item.update(order_item_params)   
       if @order_item.production_status == "now"
         @order.update(order_status: "making")
+        
+      elsif @order.order_items.count ==  @order.order_items.where(production_status: "finish").count
+            @order.update(order_status: "prepare")
       end
       
-      count = 0
-      @order_items.each do |order_item|
-        if order_item.production_status == "finish"
-          count += 1
-        end
-      end
       
-       if count == @order_items.count
-         @order.update(order_status: "wait")
-       end
+      # count = 0
+      # @order_items.each do |order_item|
+        # if order_item.production_status == "finish"
+          # count += 1
+        # end
+      # end
+      
+      # if count == @order_items.count
+        # @order.update(order_status: "wait")
+      # end
       redirect_to admin_order_path(@order.id)
   
   end
@@ -28,11 +33,11 @@ class Admin::OrderItemsController < ApplicationController
   private
   
   def order_item_params
-    params.require(:order_detail).permit(:item_id, :order_id, :quantity, :subprice, :production_status)
+    params.require(:order_item).permit(:item_id, :order_id, :quantity, :subprice, :production_status)
   end
   
-  def order_params
-    params.require(:order).permit(:status)
-  end
+  # def order_params
+    # params.require(:order).permit(:order_tatus)
+  # end
   
 end
