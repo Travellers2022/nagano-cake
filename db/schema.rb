fda7de427def25ad12c1c680861fae8093bed883
Kototap <106796386+kototap@users.noreply.github.com>
2022-08-26 07:41:15 +0000

    aboutページ、注文履歴詳細レイアウト修正

--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,10 @@ ActiveRecord::Schema.define(version: 2022_08_22_233415) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "quantity", null: false
-    t.integer "item_id", null: false
-    t.integer "customer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "item_id"
+    t.integer "customer_id"
   end
 
   create_table "customers", force: :cascade do |t|
