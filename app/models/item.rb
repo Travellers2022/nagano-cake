b3f5d3f76043f88f125125a558d2c77c989c926c
Kototap <106796386+kototap@users.noreply.github.com>
2022-08-26 11:34:37 +0000

    レイアウト修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,12 @@ class Item < ApplicationRecord
   def add_tax_price
     (self.price_without_tax * 1.10).round
   end
-  
+
   validates :name, presence: true
   validates :price_without_tax, presence: true, numericality: { only_integer: true }
   validates :is_sale, inclusion: {in: [true, false]}
   validates :genre_id, presence: true
   validates :explanation, presence: true
-  
+  validates :item_image, presence: true
+
 end
